302423c362c2c9d6e35ad8f6aad19d27fb7489c3
Tae Su Jeong <101082590+shuby-te@users.noreply.github.com>
2026-09-09 09:03:30 +0900

    Update PlayerTurnController.cpp

--- Source/Multiplayer Synchronization/PlayerTurnController.cpp
+++ Source/Multiplayer Synchronization/PlayerTurnController.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "PlayerTurnController.h"
 #include "PlayerGameMode.h"
@@ -19,6 +17,7 @@
 #include "Components/StaticMeshComponent.h"
 #include "Data/PointManager.h"
 
+//Server RPC를 통한 사격 요청
 void APlayerTurnController::OnShoot()
 {
     if (HasAuthority())
@@ -31,11 +30,13 @@ void APlayerTurnController::OnShoot()
     }
 }
 
+//Client의 사격을 Server RPC를 통해 Server로 전달
 void APlayerTurnController::Server_Shoot_Implementation()
 {
     Shoot();
 }
 
+//실제 사격 처리는 Server에서 수행하도록 구성
 void APlayerTurnController::Shoot()
 {
 
@@ -43,6 +44,7 @@ void APlayerTurnController::Shoot()
 
 }
 
+//Server에서 대상의 PlayerState를 확인하고 카드 효과 및 사격 결과를 판정한 후, 최종 결과에 따라 State를 변경
 void APlayerTurnController::Server_BlockBullet_Implementation(APlayerHandState* Target, int32 bReflected)
 {
 	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
